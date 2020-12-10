4064f4b15f02c3d196242d5e3a6bac2e6364d6d9
Ndam yeyap daniel <danielndam9@gmail.com>
2020-12-10 10:16:35 +0100

    Route

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/step/destroy/{id}', 'StepController@destroy')->name('step.destroy')
 
 Route::post('/typedossier', 'TypeDossierController@store')->name('typedossier.store')->middleware('auth');
 /**
- *Route Type de Dossiers
+ * Route Type de Dossiers
  */
 Route::post('/type/create', 'TypeDossierController@store')->name('type.store');
 Route::get('/type', 'TypeDossierController@index')->name('type.index');
@@ -67,7 +67,47 @@ Route::get('/dossier/serive', 'DossierController@dossierService')->name('dossier
  */
 Route::post('/user/create', 'HomeController@store')->name('user.store');
 Route::get('/users', 'HomeController@index')->name('user.index');
+/**
+ * Route Region
+ */
+Route::get('region/index', 'RegionController@index')->name('region.index');
+Route::patch('region/upadte/{id}', 'RegionController@update')->name('region.update');
+Route::get('region/edit/{id}', 'RegionController@edit')->name('region.edit');
+Route::post('region/create', 'RegionController@store')->name('region.store');
 
+/**
+ * Route Categorie
+ */
+Route::get('categorie/index', 'CategorieController@index')->name('categorie.index');
+Route::patch('categorie/upadte/{id}', 'CategorieController@update')->name('categorie.update');
+Route::get('categorie/edit/{id}', 'CategorieController@edit')->name('categorie.edit');
+Route::post('categorie/create', 'CategorieController@store')->name('categorie.store');
+
+/**
+ * Route District
+ */
+Route::get('district/index', 'DistrictController@index')->name('district.index');
+Route::patch('district/upadte/{id}', 'DistrictController@update')->name('district.update');
+Route::get('district/edit/{id}', 'DistrictController@edit')->name('district.edit');
+Route::post('district/create', 'DistrictController@store')->name('district.store');
+
+
+/**
+ *  Route Groupe
+*/
+Route::get('groupe/index', 'GroupeController@index')->name('groupe.index');
+Route::patch('groupe/upadte/{id}', 'GroupeController@update')->name('groupe.update');
+Route::get('groupe/edit/{id}', 'GroupeController@edit')->name('groupe.edit');
+Route::post('groupe/create', 'GroupeController@store')->name('groupe.store');
+
+
+/**
+ *  Route Structure
+*/
+Route::get('structure/index', 'StructureController@index')->name('structure.index');
+Route::patch('structure/upadte/{id}', 'StructureController@update')->name('structure.update');
+Route::get('structure/edit/{id}', 'StructureController@edit')->name('structure.edit');
+Route::post('structure/create', 'StructureController@store')->name('structure.store');
 
 
 
